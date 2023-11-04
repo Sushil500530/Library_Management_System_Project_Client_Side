76a92ca57385bfe81fa22b352328005aa464073a
Sushil97 <sushil500530@gmail.com>
2023-11-04 22:26:05 +0600

    project setup with tailwindcss and set to basic structure

---
+++ src/router/MainRouter.jsx
@@ -0,0 +1,11 @@
+
+import { createBrowserRouter } from 'react-router-dom';
+
+const MainRouter = createBrowserRouter([
+    {
+        path:'/',
+        element: <div className='text-3xl font-bold text-center my-5'>hellow world</div>
+    }
+])
+
+export default MainRouter;
